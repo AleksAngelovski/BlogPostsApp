03849f8a93a737d9bcd7736fde542b405d1cce5b
AleksAngelovski <aleksandar.angelovski1993@gmail.com>
2022-06-28 11:19:26 +0200

    added blogs and posts

---
+++ BlogPostsApp/Data/Model/Blog.cs
@@ -0,0 +1,19 @@
+﻿namespace BlogPostsApp.Data.Model
+{
+    public class Blog
+    {
+        public int Id { get; private set; }
+        public string Url { get; private set; }
+
+        public int OwnerId { get; private set; }
+
+        public string OwnerName { get; private set; }
+
+        public List<Post> Posts { get; private set; }
+
+        public Blog()
+        {
+            Posts = Posts ?? new List<Post>() { };
+        }
+    }
+}
---
+++ BlogPostsApp/Data/Model/Post.cs
@@ -0,0 +1,14 @@
+﻿namespace BlogPostsApp.Data.Model
+{
+    public class Post
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Content { get; private set; }
+        public DateTime DateCreated { get; private set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+
+    }
+}
